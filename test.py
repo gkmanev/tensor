c66cd0c8e1908704e98e6ea27a5dca7bef945c61
Georgi <gk.manev@gmail.com>
2023-10-06 13:25:40 +0300

    predict

--- test.py
+++ test.py
@@ -102,82 +102,83 @@ def on_message(client, userdata, msg):
                 
         else:        
             range = topic.split("/")[1]
-            dev = msg.payload.decode('utf-8')     
-            if dev and range:  
-                response=requests.get(url+dev).json()
-                df1=pd.DataFrame(response)
+            print(range)
+            # dev = msg.payload.decode('utf-8')     
+            # if dev and range:  
+            #     response=requests.get(url+dev).json()
+            #     df1=pd.DataFrame(response)
                 
-                if len(df1) >= 100:        
-                    series = df1[['created', 'value']]
+            #     if len(df1) >= 100:        
+            #         series = df1[['created', 'value']]
 
-                    series.drop_duplicates(subset='created', inplace=True)
-                    series['created'] = pd.to_datetime(series['created'])
+            #         series.drop_duplicates(subset='created', inplace=True)
+            #         series['created'] = pd.to_datetime(series['created'])
 
-                    date_time = pd.to_datetime(series.pop('created'), format='%d.%m.%Y %H:%M:%S')
+            #         date_time = pd.to_datetime(series.pop('created'), format='%d.%m.%Y %H:%M:%S')
 
-                    timestamp_s = date_time.map(pd.Timestamp.timestamp)
+            #         timestamp_s = date_time.map(pd.Timestamp.timestamp)
 
-                    day = 24*60*60
-                    week = day*7
+            #         day = 24*60*60
+            #         week = day*7
 
-                    series['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
-                    series['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
-                    series['Week sin'] = np.sin(timestamp_s * (2 * np.pi / week))
-                    series['Week cos'] = np.cos(timestamp_s * (2 * np.pi / week))
-                    column_indices = {name: i for i, name in enumerate(series.columns)}         
+            #         series['Day sin'] = np.sin(timestamp_s * (2 * np.pi / day))
+            #         series['Day cos'] = np.cos(timestamp_s * (2 * np.pi / day))
+            #         series['Week sin'] = np.sin(timestamp_s * (2 * np.pi / week))
+            #         series['Week cos'] = np.cos(timestamp_s * (2 * np.pi / week))
+            #         column_indices = {name: i for i, name in enumerate(series.columns)}         
 
-                    n = len(series)
-                    train_df = series[0:int(n*0.7)]
-                    val_df = series[int(n*0.7):int(n*0.9)]
-                    test_df = series[int(n*0.9):]
+            #         n = len(series)
+            #         train_df = series[0:int(n*0.7)]
+            #         val_df = series[int(n*0.7):int(n*0.9)]
+            #         test_df = series[int(n*0.9):]
 
-                    num_features = series.shape[1]
+            #         num_features = series.shape[1]
 
-                    train_mean = train_df.mean()
-                    train_std = train_df.std()
+            #         train_mean = train_df.mean()
+            #         train_std = train_df.std()
 
-                    train_df = (train_df - train_mean) / train_std
+            #         train_df = (train_df - train_mean) / train_std
 
-                    val_df = (val_df - train_mean) / train_std
-                    test_df = (test_df - train_mean) / train_std
+            #         val_df = (val_df - train_mean) / train_std
+            #         test_df = (test_df - train_mean) / train_std
 
 
-                    df_std = (series - train_mean) / train_std
-                    df_std = df_std.melt(var_name='Column', value_name='Normalized')
+            #         df_std = (series - train_mean) / train_std
+            #         df_std = df_std.melt(var_name='Column', value_name='Normalized')
                     
-                    wide_window = WindowGenerator(
-                    input_width=24, label_width=24, shift=1, train_df = train_df, val_df = val_df, test_df = test_df,
-                    label_columns=['value'])
-                    print("There!!!!!")
-                    history = compile_and_fit(lstm_model, wide_window)
+            #         wide_window = WindowGenerator(
+            #         input_width=24, label_width=24, shift=1, train_df = train_df, val_df = val_df, test_df = test_df,
+            #         label_columns=['value'])
+            #         print("There!!!!!")
+            #         history = compile_and_fit(lstm_model, wide_window)
 
-                    if history is not None:
-                        prediction_data = wide_window.make_dataset(test_df)
-                        predictions = lstm_model.predict(prediction_data)
-                        predictions_original = (predictions * train_std['value']) + train_mean['value']
-                        final_loss = history.history['loss'][-1]
-                        formatted_final_loss = "{:.2f}".format(final_loss)
-                        final_mae = history.history['mean_absolute_error'][-1]
-                        formatted_final_mae = "{:.2f}".format(final_mae)
-                        if range == 'today':            
-                            pub_topic = "forecast/"+dev+'/'+range
-                            current_datetime = datetime.now() - timedelta(days=1)
-                            # Get tomorrow's date
-                            tomorrow = current_datetime + timedelta(days=1)
-                            # Set the starting time to the beginning of tomorrow
-                            starting_time = datetime(tomorrow.year, tomorrow.month, tomorrow.day)
-                            next_day = starting_time + timedelta(days=1)
-                            for val in predictions_original[0]:
-                                date_string = starting_time.strftime("%Y-%m-%d %H:%M:%S")
-                                hour_obj = {
-                                    "date":date_string,
-                                    "power": val[0],
-                                    "loss": formatted_final_loss,
-                                    "mae": formatted_final_mae
-                                }
-                                print(hour_obj)
-                                starting_time += timedelta(hours=1)                            
-                                client.publish(pub_topic, str(hour_obj))
+            #         if history is not None:
+            #             prediction_data = wide_window.make_dataset(test_df)
+            #             predictions = lstm_model.predict(prediction_data)
+            #             predictions_original = (predictions * train_std['value']) + train_mean['value']
+            #             final_loss = history.history['loss'][-1]
+            #             formatted_final_loss = "{:.2f}".format(final_loss)
+            #             final_mae = history.history['mean_absolute_error'][-1]
+            #             formatted_final_mae = "{:.2f}".format(final_mae)
+            #             if range == 'today':            
+            #                 pub_topic = "forecast/"+dev+'/'+range
+            #                 current_datetime = datetime.now() - timedelta(days=1)
+            #                 # Get tomorrow's date
+            #                 tomorrow = current_datetime + timedelta(days=1)
+            #                 # Set the starting time to the beginning of tomorrow
+            #                 starting_time = datetime(tomorrow.year, tomorrow.month, tomorrow.day)
+            #                 next_day = starting_time + timedelta(days=1)
+            #                 for val in predictions_original[0]:
+            #                     date_string = starting_time.strftime("%Y-%m-%d %H:%M:%S")
+            #                     hour_obj = {
+            #                         "date":date_string,
+            #                         "power": val[0],
+            #                         "loss": formatted_final_loss,
+            #                         "mae": formatted_final_mae
+            #                     }
+            #                     print(hour_obj)
+            #                     starting_time += timedelta(hours=1)                            
+            #                     client.publish(pub_topic, str(hour_obj))
     except Exception as e:
         print("Error handling message:", str(e))
         traceback.print_exc()  # Print detailed error information
